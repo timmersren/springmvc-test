e90f01dfc86bbd4bbfb7d788ec318213dda4dad2
任伟 <459001452@qq.com>
2018-03-25 05:02:37 +0800

    通过注解配置springmvc的mapping和adapter

---
+++ src/cn/rain/ssm/controller/ItemsController_2.java
@@ -0,0 +1,42 @@
+package cn.rain.ssm.controller;
+
+import org.springframework.web.HttpRequestHandler;
+
+import cn.rain.ssm.model.Items;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by 左边 on 2017/5/31.
+ * 这个Handler我们实现的是HttpRequestHandler接口，
+ * 它可以被HttpRequestHandlerAdapter适配器执行。
+ */
+public class ItemsController_2 implements HttpRequestHandler {
+
+    @Override
+    public void handleRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        Items items1=new Items();
+        items1.setName("联想笔记本");
+        items1.setPrice(6000f);
+        items1.setDetail("ThinkPad T430 联想笔记本电脑！");
+
+        Items items2 = new Items();
+        items2.setName("苹果手机");
+        items2.setPrice(5000f);
+        items2.setDetail("iphone6苹果手机！");
+
+        List<Items> itemsList=new ArrayList<Items>();
+        itemsList.add(items1);
+        itemsList.add(items2);
+
+        //填充Model中的数据
+        request.setAttribute("itemsList",itemsList);
+        //请求转发到视图
+        request.getRequestDispatcher("/WEB-INF/jsp/items/itemsList.jsp").forward(request,response);
+    }
+}
---
+++ src/cn/rain/ssm/controller/ItemsController_3.java
@@ -0,0 +1,41 @@
+package cn.rain.ssm.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import cn.rain.ssm.model.Items;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by 左边 on 2017/5/31.
+ */
+@Controller // 标识这个类是一个Controller
+public class ItemsController_3 {
+	// 映射器拿到用户请求的url通过@RequestMapping(value="/queryItems")来寻找相应的Handler中的方法
+    @RequestMapping(value = "/queryItems.action") 
+    public ModelAndView queryItems(){
+        Items items1=new Items();
+        items1.setName("联想笔记本");
+        items1.setPrice(6000f);
+        items1.setDetail("ThinkPad T430 联想笔记本电脑！");
+
+        Items items2 = new Items();
+        items2.setName("苹果手机");
+        items2.setPrice(5000f);
+        items2.setDetail("iphone6苹果手机！");
+
+        List<Items> itemsList=new ArrayList<Items>();
+        itemsList.add(items1);
+        itemsList.add(items2);
+
+        ModelAndView modelAndView=new ModelAndView();
+
+        modelAndView.addObject("itemsList",itemsList);
+
+        modelAndView.setViewName("/WEB-INF/jsp/items/itemsList.jsp");
+        return modelAndView;
+    }
+}
